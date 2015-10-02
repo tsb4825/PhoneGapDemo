b79b67f105fa93a5614648f050a48a3f2aca671f
tsb4825 <tim.byars@live.com>
2015-10-02 15:58:13 -0500

    logging error

--- www/js/index.js
+++ www/js/index.js
@@ -51,7 +51,7 @@ var app = {
 
                 //Login Facebook
                 //$(document).FaceGap(config);
-                beaconService.startScanForBeacons();
+                //beaconService.startScanForBeacons();
                 
                 //Callback Login
                 function _onLogin(event) {
@@ -64,6 +64,55 @@ var app = {
                         logError("Error authenticating with Facebook");
                     }
                 }
+
+                var logToDom = function (message) {
+                    var e = document.createElement('label');
+                    e.innerText = message;
+
+                    var br = document.createElement('br');
+                    var br2 = document.createElement('br');
+                    document.body.appendChild(e);
+                    document.body.appendChild(br);
+                    document.body.appendChild(br2);
+
+                    window.scrollTo(0, window.document.height);
+                };
+
+                var delegate = new cordova.plugins.locationManager.Delegate();
+
+                delegate.didDetermineStateForRegion = function (pluginResult) {
+
+                    logToDom('[DOM] didDetermineStateForRegion: ' + JSON.stringify(pluginResult));
+
+                    cordova.plugins.locationManager.appendToDeviceLog('[DOM] didDetermineStateForRegion: '
+                        + JSON.stringify(pluginResult));
+                };
+
+                delegate.didStartMonitoringForRegion = function (pluginResult) {
+                    console.log('didStartMonitoringForRegion:', pluginResult);
+
+                    logToDom('didStartMonitoringForRegion:' + JSON.stringify(pluginResult));
+                };
+
+                delegate.didRangeBeaconsInRegion = function (pluginResult) {
+                    logToDom('[DOM] didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult));
+                };
+
+                var uuid = 'B9407F30-F5F8-466E-AFF9-25556B57FE6D';
+                var identifier = 'beaconNextToMe';
+                var minor = 53047;
+                var major = 10621;
+                var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
+
+                cordova.plugins.locationManager.setDelegate(delegate);
+
+                // required in iOS 8+
+                cordova.plugins.locationManager.requestWhenInUseAuthorization();
+                // or cordova.plugins.locationManager.requestAlwaysAuthorization()
+
+                cordova.plugins.locationManager.startMonitoringForRegion(beaconRegion)
+                    .fail(function(e) { logError(e); })
+                    .done();
             }, 2000);
         } catch (e) {
             logError(e);
