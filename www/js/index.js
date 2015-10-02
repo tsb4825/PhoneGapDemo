45bf8248abebde28f7e02642369cc968ece35e7e
tsb4825 <tim.byars@live.com>
2015-10-02 16:21:48 -0500

    trying a different plugin

--- www/js/index.js
+++ www/js/index.js
@@ -65,54 +65,15 @@ var app = {
                     }
                 }
 
-                var logToDom = function (message) {
-                    var e = document.createElement('label');
-                    e.innerText = message;
-
-                    var br = document.createElement('br');
-                    var br2 = document.createElement('br');
-                    document.body.appendChild(e);
-                    document.body.appendChild(br);
-                    document.body.appendChild(br2);
-
-                    window.scrollTo(0, window.document.height);
-                };
-
-                var delegate = new cordova.plugins.locationManager.Delegate();
-
-                delegate.didDetermineStateForRegion = function (pluginResult) {
-
-                    logError('[DOM] didDetermineStateForRegion: ' + JSON.stringify(pluginResult));
-
-                    cordova.plugins.locationManager.appendToDeviceLog('[DOM] didDetermineStateForRegion: '
-                        + JSON.stringify(pluginResult));
-                };
-
-                delegate.didStartMonitoringForRegion = function (pluginResult) {
-                    console.log('didStartMonitoringForRegion:', pluginResult);
-
-                    logError('didStartMonitoringForRegion:' + JSON.stringify(pluginResult));
-                };
-
-                delegate.didRangeBeaconsInRegion = function (pluginResult) {
-                    logError('[DOM] didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult));
-                };
-
-                var uuid = 'B9407F30-F5F8-466E-AFF9-25556B57FE6D';
-                var identifier = 'beaconNextToMe';
-                var minor = 53047;
-                var major = 10621;
-                var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
-
-                cordova.plugins.locationManager.setDelegate(delegate);
-
-                // required in iOS 8+
-                cordova.plugins.locationManager.requestWhenInUseAuthorization();
-                // or cordova.plugins.locationManager.requestAlwaysAuthorization()
-
-                cordova.plugins.locationManager.startMonitoringForRegion(beaconRegion)
-                    .fail(function(e) { logError(e); })
-                    .done();
+                estimote.beacons.startRangingBeaconsInRegion(
+                    {}, // Empty region matches all beacons.
+                    function(result) {
+                        console.log('*** Beacons ranged ***');
+                        estimote.beacons.printObject(result);
+                    },
+                    function(errorMessage) {
+                        console.log('Ranging error: ' + errorMessage);
+                    });
             }, 2000);
         } catch (e) {
             logError(e);
